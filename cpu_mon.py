cc78de640c10e7f5b7798c6aabbe06cf5c4c48c5
emylincon <emylincon@gmail.com>
2019-01-26 17:18:49 +0000

    Add files via upload

---
+++ cpu_mon.py
@@ -0,0 +1,29 @@
+import psutil
+import time
+import os
+
+cpu = []
+
+
+def get_cpu():
+    global cpu
+    prev_t = 0
+    next_t = psutil.cpu_percent(percpu=False)
+    delta = abs(prev_t - next_t)
+    prev_t = next_t
+    cpu.append(delta)
+    os.system('echo "{}," >> cpu_log.txt'.format(delta))
+    time.sleep(1)
+
+
+def main():
+    while True:
+        try:
+            get_cpu()
+        except:
+            print(cpu)
+            os.system('echo "{}" >> cpu_log.txt'.format(cpu))
+            break
+
+
+main()
